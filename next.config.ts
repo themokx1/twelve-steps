09e20553f4f91c635a0d7ce3ab35dd9fbac80024
Zoltán Palotai <zoltan.palotai@gamesforbusiness.com>
2026-03-31 22:02:03 +0200

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 import path from "node:path";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+if (process.env.NODE_ENV === "development") {
+  initOpenNextCloudflareForDev();
+}
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, "..")
 };
 
 export default nextConfig;
-
